263639e7a5eae74afd26d7359c9e3afb593725ad
Olivier Oeuillot <oeuillot@vedana.com>
2016-08-30 15:22:45 +0200

    Corrections

--- xpl-db.js
+++ xpl-db.js
@@ -31,208 +31,208 @@ var Store = Mysql;
 
 commander.command("create").action(() => {
 
-  var store = new Store(commander);
+	var store = new Store(commander);
 
-  store.create(function(error) {
-    if (error) {
-      console.error(error);
-      return;
-    }
-  });
+	store.create(function (error) {
+		if (error) {
+			console.error(error);
+			return;
+		}
+	});
 });
 
 
 commander.command("rest").action(() => {
 
-  var deviceAliases = Xpl.loadDeviceAliases(commander.deviceAliases);
-  var initCbs=[];
-  var store;
-  var memcache;
- 
-  if (commander.db) {
-    initCbs.push((callback) => {
-      store = new Store(commander, deviceAliases);
+	var deviceAliases = Xpl.loadDeviceAliases(commander.deviceAliases);
+	var initCbs = [];
+	var store;
+	var memcache;
 
-      store.connect(callback);
-    });
-  }
+	if (commander.db) {
+		initCbs.push((callback) => {
+			store = new Store(commander, deviceAliases);
 
-  if (commander.memcached) {
-    initCbs.push((callback) => {
-      memcache = new Memcache(commander, deviceAliases);
+			store.connect(callback);
+		});
+	}
 
-      memcache.initialize((error) => {
-        if (error) {
-          return callback(error);
-        }
-        
-        callback();
-      });
-    });
-  }
+	if (commander.memcached) {
+		initCbs.push((callback) => {
+			memcache = new Memcache(commander, deviceAliases);
 
-  Async.parallel(initCbs, (error) => {
-    if (error) {
-      console.error(error);
-      return;
-    }
-    
-    var f = (xpl) => {
-      var server = new Server(commander, store, xpl, memcache);
-    
-      server.listen((error, server) => {
-        if (error) {
-          console.error(error);
-        }
+			memcache.initialize((error) => {
+				if (error) {
+					return callback(error);
+				}
 
-        if (memcache && store) {
-          process.on('exit', () => {
-            memcache.saveRestServerURL('', (error) => {
-              debug("xpl-db", "Reset rest server URL into memcache !");
-            });
-          });
-        }
-        
-        var url="http://"+ ip.address() + ":"+ server.address().port;
-        debug("xpl-db", "Set rest server url to",url);
-        if (!memcache) {
-          return;
-        }
-        memcache.saveRestServerURL(url, (error) => {
-          if (error) {
-            console.error(error);
-          }
-        });
-      });
-    };
-    
-    var xpl = new Xpl(commander);
+				callback();
+			});
+		});
+	}
 
-    xpl.on("error", (error) => {
-      console.error("XPL error", error);
-    });
+	Async.parallel(initCbs, (error) => {
+		if (error) {
+			console.error(error);
+			return;
+		}
 
-    xpl.bind((error) => {
-      if (error) {
-        console.error("Can not open xpl bridge ", error);
-        process.exit(2);
-        return;
-      }
-      
-      f(xpl);
-    });    
-  });
+		var f = (xpl) => {
+			var server = new Server(commander, store, xpl, memcache);
+
+			server.listen((error, server) => {
+				if (error) {
+					console.error(error);
+				}
+
+				if (memcache && store) {
+					process.on('exit', () => {
+						memcache.saveRestServerURL('', (error) => {
+							debug("xpl-db", "Reset rest server URL into memcache !");
+						});
+					});
+				}
+
+				var url = "http://" + ip.address() + ":" + server.address().port;
+				debug("xpl-db", "Set rest server url to", url);
+				if (!memcache) {
+					return;
+				}
+				memcache.saveRestServerURL(url, (error) => {
+					if (error) {
+						console.error(error);
+					}
+				});
+			});
+		};
+
+		var xpl = new Xpl(commander);
+
+		xpl.on("error", (error) => {
+			console.error("XPL error", error);
+		});
+
+		xpl.bind((error) => {
+			if (error) {
+				console.error("Can not open xpl bridge ", error);
+				process.exit(2);
+				return;
+			}
+
+			f(xpl);
+		});
+	});
 });
 
 commander.command("store").action(() => {
 
-  var deviceAliases = Xpl.loadDeviceAliases(commander.deviceAliases);
-  
-  debug("store", "Store starting ... deviceAliases=",deviceAliases);
+	var deviceAliases = Xpl.loadDeviceAliases(commander.deviceAliases);
 
-  var store;
-  var memcache;
-  var initCbs=[];
-  
-  if (commander.db) {
-    initCbs.push((callback) => {
-      store = new Store(commander, deviceAliases);
+	debug("store", "Store starting ... deviceAliases=", deviceAliases);
 
-      store.connect((error) => {
-        if (error) {
-          debug("store", "Store error=",error);
-          return callback(error);
-        }
-        
-        debug("store", "Store connected");
-        
-        callback();
-      });
-    });
-  }
-  if (commander.memcached) {
-    initCbs.push((callback) => {
-      memcache = new Memcache(commander, deviceAliases);
+	var store;
+	var memcache;
+	var initCbs = [];
 
-      memcache.initialize((error) => {
-        if (error) {
-          debug("store", "Memcached error=",error);
-          return callback(error);
-        }
-        
-        debug("store", "Memcached connected");
-        
-        callback();
-      });
-    });
-  }
-  
-  Async.parallel(initCbs, (error) => {
-    if (error) {
-      debug("store", "Initialization error=",error);
-      console.error(error);
-      return;
-    }
-    debug("store", "Initialization OK");
-    
-    try {
-      if (!commander.xplSource) {
-        var hostName = os.hostname();
-        if (hostName.indexOf('.') > 0) {
-          hostName = hostName.substring(0, hostName.indexOf('.'));
-        }
+	if (commander.db) {
+		initCbs.push((callback) => {
+			store = new Store(commander, deviceAliases);
 
-        commander.xplSource = "db." + hostName;
-      }
+			store.connect((error) => {
+				if (error) {
+					debug("store", "Store error=", error);
+					return callback(error);
+				}
 
-      var xpl = new Xpl(commander);
+				debug("store", "Store connected");
 
-      xpl.on("error", (error) => {
-        console.error("XPL error", error);
-      });
+				callback();
+			});
+		});
+	}
+	if (commander.memcached) {
+		initCbs.push((callback) => {
+			memcache = new Memcache(commander, deviceAliases);
 
-      xpl.bind((error) => {
-        if (error) {
-          console.error("Can not open xpl bridge ", error);
-          process.exit(2);
-          return;
-        }
+			memcache.initialize((error) => {
+				if (error) {
+					debug("store", "Memcached error=", error);
+					return callback(error);
+				}
 
-        debug("store", "Xpl bind succeed ");
+				debug("store", "Memcached connected");
 
-        var processMessage = (message) => {
+				callback();
+			});
+		});
+	}
 
-          if (message.bodyName === "sensor.basic") {
-            if (store) {
-              store.save(message, (error) => {
-                if (error) {
-                  console.error('error connecting: ', error, error.stack);
-                  return;
-                }
-              });
-            }
-            if (memcache) {
-              memcache.saveMessage(message);
-            }
-            
-            return;
-          }
-        };
-        
-        xpl.on("xpl:xpl-trig", processMessage);
-        xpl.on("xpl:xpl-stat", processMessage);
+	Async.parallel(initCbs, (error) => {
+		if (error) {
+			debug("store", "Initialization error=", error);
+			console.error(error);
+			return;
+		}
+		debug("store", "Initialization OK");
 
-        /*
-         * xpl.on("message", (message, packet, address) => {
-         * 
-         * });
-         */
+		try {
+			if (!commander.xplSource) {
+				var hostName = os.hostname();
+				if (hostName.indexOf('.') > 0) {
+					hostName = hostName.substring(0, hostName.indexOf('.'));
+				}
 
-      });
-    } catch (x) {
-      console.error(x);
-    }
-  });
+				commander.xplSource = "db." + hostName;
+			}
+
+			var xpl = new Xpl(commander);
+
+			xpl.on("error", (error) => {
+				console.error("XPL error", error);
+			});
+
+			xpl.bind((error) => {
+				if (error) {
+					console.error("Can not open xpl bridge ", error);
+					process.exit(2);
+					return;
+				}
+
+				debug("store", "Xpl bind succeed ");
+
+				var processMessage = (message) => {
+
+					if (message.bodyName === "sensor.basic") {
+						if (store) {
+							store.save(message, (error) => {
+								if (error) {
+									console.error('error connecting: ', error, error.stack);
+									return;
+								}
+							});
+						}
+						if (memcache) {
+							memcache.saveMessage(message);
+						}
+
+						return;
+					}
+				};
+
+				xpl.on("xpl:xpl-trig", processMessage);
+				xpl.on("xpl:xpl-stat", processMessage);
+
+				/*
+				 * xpl.on("message", (message, packet, address) => {
+				 *
+				 * });
+				 */
+
+			});
+		} catch (x) {
+			console.error(x);
+		}
+	});
 });
 
 commander.parse(process.argv);
